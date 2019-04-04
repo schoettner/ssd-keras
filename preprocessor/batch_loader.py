35be0b608fab8b90fa00814b3f57a7b0b09e03e3
bastian <bastian.schoettner@rewe-digital.com>
2019-04-05 00:21:24 +0200

    add batch loader signatures

--- preprocessor/batch_loader.py
+++ preprocessor/batch_loader.py
@@ -2,3 +2,30 @@ class BatchLoader(object):
     """
     load a batch of x, y
     """
+
+    def __init__(self, files: list, batch_size: int = 8, apply_shuffle: bool = True):
+        self.apply_shuffle = apply_shuffle
+        self.batch_size = batch_size
+        self.files = files
+
+    @staticmethod
+    def load_image(img: str, img_width: int = 300, img_height: int = 300):
+        """
+        load the image file. resize it to the desired resolution and put into correct input format (float32) for the model
+        :param img: the path to the image file
+        :return: numpy array of the image in shape=(width, height, 3) and dtype=float32
+                 or None if the file could not be loaded
+        """
+        return None
+
+    @staticmethod
+    def load_label(label: str):
+        """
+        load the label text file with the bounding box details. one file can contain multiple boxes
+        input format in file class_1, x_min_1, y_min_1, x_max_1, y_max_1, class_2, ...
+        the label will be processed by the encoder later
+        :param label: path to the text file
+        :return: numpy array of the labels in shape=(num_boxes, 5) and dtpye=int32
+                 or None if the file could not be loaded
+        """
+        return None
